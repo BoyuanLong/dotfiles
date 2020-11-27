410806f67b6c8d018c9195a122cfd4c8f3b9de85
longboyuan <longboyuan@bytedance.com>
2020-11-27 15:43:57 +0800

    My first dotfiles commit

---
+++ shell/aliases.sh
@@ -0,0 +1,55 @@
+# Use colors
+alias ls='ls -G'
+alias grep='grep -G'
+
+# ls aliases
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+
+# Aliases to protect agianst overwritting
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Create a directory and cd into it
+mcd() {
+    mkdir "${1}" && cd "${1}"
+}
+
+# Jump to directory containing file
+jump() {
+    cd "$(dirname ${1})"
+}
+
+# Go up [n] directories
+up()
+{
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i=0; i<${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
+}
+
+# Execute a command in a specific directory
+# cd into ${1}
+# left shift all arguments, losing ${1}
+# execute the rest of the arguments
+xin() {
+    (
+        cd "${1}" && shift && "${@}"
+    )
+}

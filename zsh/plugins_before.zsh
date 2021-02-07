104269fa764993afe755ec71cc5278c0c9619309
Boyuan Long <boyuanlo@usc.edu>
2021-02-07 16:20:15 +0800

    add zsh smartcase

--- zsh/plugins_before.zsh
+++ zsh/plugins_before.zsh
@@ -3,6 +3,8 @@
 # zsh-completions
 fpath=(~/.dotfiles/zsh/plugins/zsh-completions/src $fpath)
 
+zstyle ':completion:*' matcher-list '' '+m:{a-zA-Z}={A-Za-z}' '+r:|[._-]=* r:|=*' '+l:|=* r:|=*'
+
 # zsh-autosuggestions
 source ~/.dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=241"
